d10fbbfe2e2f8ea0b53e5cad6bb605e59743a307
Tony Zhou <beyondjay@gmail.com>
2015-06-08 16:06:39 +0800

    add first leetcodes

---
+++ Leetcode/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    /*
+    Given a binary tree, return the inorder traversal of its nodes' values.
+
+    For example:
+    Given binary tree {1,#,2,3},
+       1
+        \
+         2
+        /
+       3
+    return [1,3,2].
+
+    Note: Recursive solution is trivial, could you do it iteratively?*/
+
+    /**
+    * Definition for binary tree
+    */
+      public class TreeNode {
+          public int val;
+          public TreeNode left;
+          public TreeNode right;
+          public TreeNode(int x) { val = x; }
+      }
+ 
+    public partial class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            List<int> result = new List<int>();
+
+            TreeNode p = root;
+
+            while (p != null || stack.Any())
+            {
+                while (p != null) //handle the left node
+                {
+                    stack.Push(p);
+                    p = p.left;
+                }
+
+                if (stack.Any())
+                {
+                    p = stack.Pop();
+                    result.Add(p.val);
+                    p = p.right;
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Leetcode/GenerateParentheses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    /*
+    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+    For example, given n = 3, a solution set is:
+
+    "((()))", "(()())", "(())()", "()(())", "()()()"
+    */
+
+    public partial class Solution
+    {
+        private IList<string> result = new List<string>();
+        public IList<string> GenerateParenthesis(int n)
+        {
+            if (n < 1)
+                return null;
+
+            CombineParenthesis(string.Empty, 0, 0, n);
+
+            return result;
+        }
+
+        private void CombineParenthesis(string partString, int leftParenthesisNumber, int rightParenthesisNumber, int n)
+        {
+            if (leftParenthesisNumber == n && rightParenthesisNumber == n)
+            {
+                result.Add(partString);
+            }
+            else
+            {
+                if (leftParenthesisNumber < n)
+                {
+                    CombineParenthesis(partString + "(", leftParenthesisNumber + 1, rightParenthesisNumber, n);
+                }
+
+
+                if (rightParenthesisNumber < leftParenthesisNumber)
+                {
+                    CombineParenthesis(partString + ")", leftParenthesisNumber, rightParenthesisNumber + 1, n);
+                }
+            }
+
+        }
+    }
+}
